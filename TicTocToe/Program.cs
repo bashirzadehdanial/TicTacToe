2a03b24ffa24b3c0d7ddeb330c37ece43e4ba738
Danial <bashirzadehdanial@gmail.com>
2024-06-30 20:59:43 +0100

    IsMoveValid function is added

--- TicTocToe/Program.cs
+++ TicTocToe/Program.cs
@@ -70,5 +70,21 @@ namespace TicTacToe
                 }
             }
         }
+        static bool IsMoveValid(int choice)
+        {
+            switch (choice)
+            {
+                case 1: return board[0, 0] == '1';
+                case 2: return board[0, 1] == '2';
+                case 3: return board[0, 2] == '3';
+                case 4: return board[1, 0] == '4';
+                case 5: return board[1, 1] == '5';
+                case 6: return board[1, 2] == '6';
+                case 7: return board[2, 0] == '7';
+                case 8: return board[2, 1] == '8';
+                case 9: return board[2, 2] == '9';
+                default: return false;
+            }
+        }
     }
 }
