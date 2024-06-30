53804d37f9e49a8d77bbd43814a60bff72d0c973
Danial <bashirzadehdanial@gmail.com>
2024-06-30 20:05:23 +0100

    Add initial TicTacToe code

--- TicTocToe/Program.cs
+++ TicTocToe/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static char[,] board = new char[3,3]
+        {
+            {'1','2','3'},
+            {'4','5','6'},
+            {'7','8','9'}
+        };
+
+        static void Main(string[] args)
+        {
+            int player = 1;
+            int result;
+        }
+    }
+}
